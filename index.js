f9229cb09f8d89b58ab12889c0a7d50929971aa1
tanishkagupta2207 <tanishkagupta2207@gmail.com>
2025-01-26 02:48:02 +0530

    adding models and routes

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ connectToMongoDB();
 const app = express()
 const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Tanishka here!')
-})
+//Available routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)

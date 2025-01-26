778127cc54a6e4e6ba618a94b141da4d8a3c4c99
tanishkagupta2207 <tanishkagupta2207@gmail.com>
2025-01-27 01:25:19 +0530

    update note

--- routes/notes.js
+++ routes/notes.js
@@ -43,4 +43,57 @@ router.post(
   }
 );
 
+//Route 2 Update a note for a user. Login Required
+router.put(
+  "/updateNote/:id",
+  fetchUser,
+  [
+    body("title", "Enter a valid title").isLength({ min: 3 }),
+    body("description", "Description must be atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const { title, description, tag } = req.body;
+
+      const newNote = {};
+      if (title) {
+        newNote.title = title;
+      }
+      if (description) {
+        newNote.description = description;
+      }
+      if (tag) {
+        newNote.tag = tag;
+      }
+
+      //find the note to update
+      let note = await Notes.findById(req.params.id);
+
+      if (!note) {
+        res.status(404).send("Note not Found !");
+      }
+      if (note.user.toString() !== req.user.id) {
+        return res.status(401).send("Action not allowed");
+      }
+
+      note = await Notes.findByIdAndUpdate(
+        req.params.id,
+        { $set: newNote },
+        { new: true }
+      );
+
+      res.json({ note });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Internal error");
+    }
+  }
+);
+
 module.exports = router;

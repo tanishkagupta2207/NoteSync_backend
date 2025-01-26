3573297a447a18e273d01ba7b50dd0ea6f571aad
tanishkagupta2207 <tanishkagupta2207@gmail.com>
2025-01-26 17:19:26 +0530

    creating user

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,14 @@
+const express = require('express');
 const router = require('express').Router();
+const User = require('../models/User');
 
-router.get('/', (req, res) => {
-    res.json({message: 'Hello from auth route'});
+
+//Create a User using: POST "/api/auth/". No login required
+router.post('/', (req, res) => {
+    console.log(req.body);
+    const user = User(req.body);
+    user.save();
+    res.json({message: 'User created successfully',"user":user});
 });
 
 module.exports = router;
